89c98ed8d194dd12a471b0ae0c5888668b375b9d
mluc404 <199426019+mluc404@users.noreply.github.com>
2025-03-12 14:06:46 -0700

    Display books in cards dynamically

--- script.js
+++ script.js
@@ -18,3 +18,30 @@ addBookToLibrary("Harry Potter", "JK Rowling", 999, true);
 addBookToLibrary("Rum Diary", "Hunter Thompson", 345, false);
 addBookToLibrary("Paris", "Marc Levi", 212, false);
 addBookToLibrary("Stoic", "Marcus Aureli", 467, false);
+
+// Function to display books
+let container = document.querySelector(".container");
+let bookCard = document.querySelector(".bookCard");
+
+function displayBook(arr) {
+  arr.forEach((book) => {
+    let eachBook = document.createElement("div");
+    eachBook.setAttribute("class", "bookCard");
+    eachBook.innerHTML = bookCard.innerHTML;
+
+    let title = eachBook.querySelector(".title");
+    title.textContent = `Title: ${book.title}`;
+
+    let author = eachBook.querySelector(".author");
+    author.textContent = `Author: ${book.author}`;
+
+    let pages = eachBook.querySelector(".pages");
+    pages.textContent = `Pages: ${book.pages}`;
+
+    let checkRead = eachBook.querySelector(".read");
+    checkRead.textContent = `Read: ${book.read}`;
+
+    container.appendChild(eachBook);
+  });
+}
+displayBook(myLibrary);
